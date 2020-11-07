6efb59211dc65c7380a1b92b05e5ec249a154ad3
Daniel Lucumi <dflm25@gmail.com>
2020-11-06 21:28:57 -0500

    I just add second route size.js

--- frontend/components/admin/SideBar.js
+++ frontend/components/admin/SideBar.js
@@ -2,6 +2,7 @@
  * Sidebar
  */
 import React from 'react';
+import Link from 'next/link'
 
 const SideBar = () => {
 
@@ -9,21 +10,24 @@ const SideBar = () => {
     <div className="main-sidebar">
       <aside id="sidebar-wrapper">
         <div className="sidebar-brand">
-          <a href="index.html">Stisla</a>
+          <a href="index.html">Geek</a>
         </div>
         <div className="sidebar-brand sidebar-brand-sm">
-          <a href="index.html">St</a>
+          <a href="index.html">Gk</a>
         </div>
         <ul className="sidebar-menu">
             <li className="menu-header">Dashboard</li>
-            <li className="nav-item dropdown">
-              <a href="#" className="nav-link has-dropdown"><i className="fas fa-fire"></i><span>Dashboard</span></a>
-              <ul className="dropdown-menu">
-                <li><a className="nav-link" href="index-0.html">General Dashboard</a></li>
-                <li><a className="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-              </ul>
+            <li className="nav-item">
+              <Link href="/admin" className="nav-link">
+                <a><i className="fas fa-fire"></i><span>Dashboard</span></a>
+              </Link>
             </li>
-            <li className="menu-header">Starter</li>
+            <li className="menu-header">Administrators</li>
+            <li className="nav-item">
+              <Link href="/admin/size" className="nav-link">
+                <a><i className="fas fa-fire"></i><span>Sizes</span></a>
+              </Link>
+            </li>
           </ul>
       </aside>
     </div>
